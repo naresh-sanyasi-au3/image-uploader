ef828e5277fd3a0c19f379087e43014fa18f79e0
naresh-sanyasi-au3 <naresspp@gmail.com>
2021-03-19 20:38:54 +0530

    submitted

--- server/app.js
+++ server/app.js
@@ -8,6 +8,11 @@ require('dotenv').config();
 const port = process.env.PORT || 5000
 
 app.use(cors());
+app.use(express.static('client/build'));
+const path = require('path');
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+});
 
 mongoose
     .connect(process.env.DATABASE, {
@@ -54,13 +59,7 @@ app.post('/uploads', uploadImage.array('image', 25), function (req, res, next) {
         .catch(err => res.status(400).json('error: ' + err));
         console.log("image uploaded: " + newImage)
 });
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static('build'));
-    const path = require('path');
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-};
+
 
 
 
